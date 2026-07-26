3e8a8d0bc19a7f8d45001864b07b40bd5dd9d8d2
Ioana <ioana3popescu@gmail.com>
2026-07-26 21:40:09 +0200

    test: add tests for MultiplicationTable class

--- src/test/java/com/MultiplicationTableTest.java
+++ src/test/java/com/MultiplicationTableTest.java
@@ -1,11 +1,11 @@
 package com;
 
 import com.ioana.MultiplicationTable;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MultiplicationTableTest {
     @ParameterizedTest
@@ -16,4 +16,9 @@ public class MultiplicationTableTest {
         assertEquals(givenNumber * i, table.multiply(i));
     }
 
+    @Test
+    void testCreateTable() {
+        MultiplicationTable table = new MultiplicationTable(5);
+        assertDoesNotThrow(table::createTable);
+    }
 }
